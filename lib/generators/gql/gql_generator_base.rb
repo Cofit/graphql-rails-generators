b66a248297ac614cb00132fd68998d3420b06ec4
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2020-03-18 10:59:15 +0800

    add type

--- lib/generators/gql/gql_generator_base.rb
+++ lib/generators/gql/gql_generator_base.rb
@@ -6,6 +6,7 @@ module Gql
 
     included do
       protected
+
       def type_map
         {
           integer: 'Int',
@@ -16,19 +17,20 @@ module Gql
           date: 'Types::DateType',
           hstore: 'Types::JSONType',
           text: 'String',
-          json: 'Types::JSONType'
+          json: 'Types::JSONType',
+          jsonb: 'Types::JSONType'
         }
       end
-  
+
       def map_model_types(model_name)
         klass = model_name.constantize
         associations = klass.reflect_on_all_associations(:belongs_to)
         bt_columns = associations.map(&:foreign_key)
-  
+
         klass.columns
           .reject { |col| bt_columns.include?(col.name) }
           .reject { |col| type_map[col.type].nil? }
-          .map { |col| {name: col.name, gql_type: type_map[col.type]} }
+          .map { |col| { name: col.name, gql_type: type_map[col.type] } }
       end
     end
   end
