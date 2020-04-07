25cdacb6a4e26b6659861c6561acb53071cddca3
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2020-04-07 18:05:38 +0800

    update generator

--- lib/generators/gql/input_generator.rb
+++ lib/generators/gql/input_generator.rb
@@ -4,12 +4,12 @@ module Gql
     include GqlGeneratorBase
     source_root File.expand_path('../templates', __FILE__)
     argument :model_name, type: :string
-  
+
     def generate_input_type
       file_name = model_name
 
       ignore = ['id', 'created_at', 'updated_at']
-      @fields = map_model_types(model_name).reject { |field| ignore.include?(field[:name]) }
+      @fields = map_model_types(model_name).reject { |field| (field[:name] == "id") }
 
       template('input_type.rb', "app/graphql/types/input/#{file_name.underscore}_input.rb")
     end
